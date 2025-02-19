d6495b1f7983567501cb86e3e5895a2d815c3b75
Eleazar Gamez <eleazar.gamezd@gmail.com>
2025-02-19 13:20:17 -0500

    first commit

---
+++ src/app/core/services/globalHttp/global-http.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, from, lastValueFrom, map } from 'rxjs';
+import { RequestMethod } from '../../enums/httpRequest/requestMethods.enum';
+import { ApiResponse } from '../../interfaces/api/api-response.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GlobalHttpService {
+  constructor(public _http: HttpClient) {}
+
+  public async makeRequest<T, P>(
+    route: string,
+    payload: P,
+    method: string = RequestMethod.GET
+  ): Promise<T> {
+    return lastValueFrom(
+      from(this.makeHttpRequest<ApiResponse<T>>(route, payload, method)).pipe(
+        map((res: ApiResponse<T>) => res.data),
+        catchError((error: HttpErrorResponse) => {
+          console.error('Error:', error);
+          throw error;
+        })
+      )
+    );
+  }
+
+  public async makeHttpRequest<T>(
+    url: string,
+    options: unknown = {},
+    method: string = RequestMethod.GET
+  ): Promise<T> {
+    const requestOptions: object =
+      method === RequestMethod.GET ? { body: options } : {};
+    return lastValueFrom(
+      this._http
+        .request<T>(method, url, requestOptions)
+        .pipe(map((response) => response as T))
+    );
+  }
+}
